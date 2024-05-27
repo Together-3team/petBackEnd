132ce61dd6a35ab09533422711f936f2a89bfc30
SENGJUN <49616211+tmdwns1521@users.noreply.github.com>
2024-05-27 12:53:41 +0900

    ENV 설정

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,11 +4,11 @@ config()
 
 export const AppDataSource = new DataSource({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1234',
-    database: '3team',
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
     logging: true,
     synchronize: true,
     entities: ['src/entities/*.ts']
