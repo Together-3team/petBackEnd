b9774217af2399fb4c14c9960d0b6341c4ea165b
jeonghun <go891017@gmail.com>
2024-06-22 22:00:48 +0900

    feat: zzim response 정리

--- src/services/zzim.service.ts
+++ src/services/zzim.service.ts
@@ -1,5 +1,5 @@
 import { DeleteResult } from 'typeorm'
-import { ZzimCreateRequestDto, ZzimResponseDto } from '../dtos'
+import { HomeProductResponseDto, ZzimCreateRequestDto, ZzimResponseDto, ProductResponseDto } from '../dtos'
 import { Zzim, User } from '../entities'
 import { ZzimRepository } from '../repositories'
 import { plainToInstance } from 'class-transformer'
@@ -11,28 +11,21 @@ export class ZzimService {
     this.zzimRepository = new ZzimRepository()
   }
 
-  public getZzimById = async (zzimId: string, user: User): Promise<ZzimResponseDto> => {
-    const zzim = this.zzimRepository.findZzimById(parseInt(zzimId), user)
-    return plainToInstance(ZzimResponseDto, zzim)
-  }
-
-  public getZzimsByUser = async (user: User): Promise<ZzimResponseDto[]> => {
-    const zzims = await this.zzimRepository.findZzimsByUser(user)
-    return zzims.map(zzim => plainToInstance(ZzimResponseDto, zzim))
-  }
-
   public createZzim = async (zzimData: ZzimCreateRequestDto, user: User): Promise<ZzimResponseDto> => {
     const Zzim = await this.zzimRepository.findZzimByUserAndProductId(zzimData.productId, user)
     if (Zzim) throw new Error("해당 상품의 위시리스트가 존재합니다")
-    const zzim = this.zzimRepository.createZzim(zzimData, user)
-    return plainToInstance(ZzimResponseDto, zzim)
+    const zzim = await this.zzimRepository.createZzim(zzimData, user)
+    return plainToInstance(ZzimResponseDto, {...zzim, product: plainToInstance(ProductResponseDto, zzim.product)})
   }
 
-  public deleteZzim = (zzimId: string): Promise<DeleteResult> => {
-    return this.zzimRepository.deleteZzim(parseInt(zzimId))
+  public deleteZzim = (productId: string): Promise<DeleteResult> => {
+    return this.zzimRepository.deleteZzim(parseInt(productId))
   }
 
-  public getZzimedProducts = (user: User) => {
-    return this.zzimRepository.getZzimedProducts(user)
+  public getZzimedProducts = async (user: User) => {
+    const zzims = await this.zzimRepository.getZzimedProducts(user)
+    const entities = zzims.entities
+    const raw = zzims.raw
+    return Promise.all(entities.map(async (zzim, i) => plainToInstance(HomeProductResponseDto, {...zzim.product, averageRating: parseFloat(raw[i].averageRating), reviewCount: parseInt(raw[i].reviewCount), totalAmount: parseInt(raw[i].totalAmount)/(parseInt(raw[i].reviewCount) || 1), isZzimed: true})))
   }
 }
